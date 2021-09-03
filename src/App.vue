68863c8cf9cffb9518459f339bd6589add1bc50d
maldrek3103 <cpisanu96@gmail.com>
2021-09-03 16:39:09 +0200

    Made title

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Title />
+  </div>
 </template>
 
 <script>
+import Title from "@/components/Title.vue";
 export default {
   name: "App",
+  components: {
+    Title,
+  },
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
 </style>
--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,7 +1,8 @@
-<template></template>
+<template>
+  <h1>La mia prima app con Vue CLI</h1>
+</template>
 
 <script>
-export default {};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
