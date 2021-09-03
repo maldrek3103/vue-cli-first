ce1ccfc69abcc2e453076835fc1dbe7fc3e736d0
maldrek3103 <cpisanu96@gmail.com>
2021-09-03 16:47:43 +0200

    Added jumbotron

---
+++ src/components/Hero.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <img src="../assets/jumbotron.jpg" />
+  </div>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img {
+  width: 100vh;
+  height: 90vh;
+}
+</style>
